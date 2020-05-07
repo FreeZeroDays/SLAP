50befa76deb937836c3d83235403e15b7c6833eb
Derrick Song <root@derricksong.com>
2020-05-06 22:40:36 -0700

    added ranges.conf, rewrote slap.sh to support ranges.conf based on demonstrated format

--- slap.sh
+++ slap.sh
@@ -1,17 +1,34 @@
 #!/bin/sh
 # Specify targets to scan in <HOST>, edit channel (SLAP = SLack + nmAP), and set OAuth Access Token to begin SLAPping. 
 # Edits will be made as time goes on - removed -sV and other flags to lower aggression.
-TARGETS="<HOST>"
-OPTIONS="-v -p- -Pn -T4"
-date=$(date +%Y-%m-%d-%H-%M-%S)
-cd /nmap/diffs
-nmap $OPTIONS $TARGETS -oA scan-$date > /dev/null
-slack(){
-curl -F file=@diff-$date -F initial_comment="Port Change Detected" -F channels=#slap -F token=<TOKEN> https://slack.com/api/files.upload
-}
 
-if [ -e scan-prev.xml ]; then
-ndiff scan-prev.xml scan-$date.xml > diff-$date
-[ "$?" -eq "1" ] && sed -i -e 1,3d diff-$date && slack
-fi
-ln -sf scan-$date.xml scan-prev.xml
+SCANOPTIONS="-v -p- -Pn -T4"
+TARGETFILE="ranges.conf"
+DATE=$(date +%Y-%m-%d-%H-%M-%S)
+WORKINGDIR="/nmap/diffs"
+SLACKTOKEN=""
+SLACKCOMMENT=""
+
+while read line; do
+	CUSTOMERNAME=`echo ${line} | cut -d: -f1`
+	IPRANGES=`echo ${line} | cut -d: -f2`
+	echo "Scanning ${CUSTOMERNAME}'s perimeter, detected IP ranges: ${IPRANGES}"
+	cd ${WORKINGDIR}
+	nmap ${OPTIONS} ${IPRANGES} -oX ${CUSTOMERNAME}-${DATE}.xml > /dev/null
+
+	slack() {
+		curl \
+		-F file=@${CUSTOMERNAME}-${DATE}-diff \
+	       	-F initial_comment="Port Change Detected" \
+		-F channels=#slap \
+		-F token=${SLACKTOKEN} \
+	       	https://slack.com/api/files.upload
+	}
+
+	if [ -e ${CUSTOMERNAME}-prev.xml ]; then
+		ndiff ${CUSTOMERNAME}-prev.xml ${CUSTOMERNAME}-${DATE}.xml > ${CUSTOMERNAME}-${DATE}-diff
+		[ "$?" -eq "1" ] && sed -i -e 1,3d ${CUSTOMERNAME}-${DATE} && slack
+	fi
+	ln -sf ${CUSTOMERNAME}-${DATE}.xml ${CUSTOMERNAME}-prev.xml
+
+done < ${TARGETFILE}
