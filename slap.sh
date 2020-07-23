65fc46846ec8c6d2e18200d3f812eccd947ab7c5
Derrick Song <root@derricksong.com>
2020-07-23 15:31:01 -0700

    enriched the slack notification message for new ports detected, changed the if condition so only a port discovered message will occur or a no port discovered message will occur

--- slap.sh
+++ slap.sh
@@ -12,15 +12,15 @@ while read line; do
         echo "Scanning ${CUSTOMERNAME}'s perimeter, detected IP ranges: ${IPRANGES}"
         cd ${WORKINGDIR}
         nmap ${SCANOPTIONS} ${IPRANGES} -oX ${CUSTOMERNAME}-${DATE}.xml > /dev/null
-        slackUpload() {
+        slack_report() {
                 curl \
                 -F file=@${CUSTOMERNAME}-${DATE}-diff \
-                -F initial_comment="A port has been discovered!" \
+                -F initial_comment=${MESSAGE} \
                 -F channels=#slap \
                 -F token=${SLACKTOKEN} \
                 https://slack.com/api/files.upload
         }
-        slackMessage() {
+        no_diff() {
                 curl \
                 -X POST \
                 -H "Authorization: Bearer "${SLACKTOKEN}"" \
@@ -30,8 +30,16 @@ while read line; do
         }
         if [ -e ${CUSTOMERNAME}-prev.xml ]; then
                 ndiff ${CUSTOMERNAME}-prev.xml ${CUSTOMERNAME}-${DATE}.xml > ${CUSTOMERNAME}-${DATE}-diff
-                [ "$?" -eq "1" ] && sed -i -e 1,3d ${CUSTOMERNAME}-${DATE}-diff && slackUpload
+                if [ "$?" -eq "1" ]; then
+			sed -i -e 1,3d ${CUSTOMERNAME}-${DATE}-diff
+			IPADDRS=$(cat ${CUSTOMERNAME}-${DATE}-diff | grep -B1 "+Host is up" | egrep -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | tr '\n' ', ')
+			PORTCOUNT=$(cat ${CUSTOMERNAME}-${DATE}-diff | egrep "^\+[0-9]{1,5}/tcp" | wc -l)
+			MESSAGE="[${CUSTOMERNAME}]: ${PORTCOUNT} ports were detected across the following IP addresses: ${IPADDRS}"
+			slack_report
+		elif [ "$?" -eq "0" ]; then
+			no_diff
+		fi
+
         fi
         ln -sf ${CUSTOMERNAME}-${DATE}.xml ${CUSTOMERNAME}-prev.xml
 done < ${TARGETFILE}
-slackMessage
